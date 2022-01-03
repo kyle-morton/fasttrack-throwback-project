5aea8a695fd6335290b37ba550571c9006d7f37e
Kyle Morton <kylemortondevelopment@gmail.com>
2022-01-02 22:09:40 -0600

    adding in service stubs

---
+++ DangGlider.Web.Core/Services/FlightService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangGlider.Web.Core.Services
+{
+    internal class FlightService
+    {
+    }
+}
